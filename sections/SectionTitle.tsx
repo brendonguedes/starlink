831d453ba4215819e40bb6c465dba15daaf74231
Brendon Guedes <brendonguedes@icloud.com>
2022-12-27 23:55:08 -0300

    no used prop

--- sections/SectionTitle.tsx
+++ sections/SectionTitle.tsx
@@ -3,7 +3,7 @@ export interface Props {
   subTitle: string;
 }
 
-export default function SectionTitle({ themeTitle, title, subTitle }: Props) {
+export default function SectionTitle({ title, subTitle }: Props) {
   return (
     <div class="w-full max-w-screen-2xl m-auto mt-8 px-6 bg-black">
       <div class="bg-gradient-15 from-blue-dark to-blue-super-light h-14 w-36 rounded-full flex justify-center items-center m-auto">
